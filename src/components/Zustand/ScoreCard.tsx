595f12bbf766b3b9b264869accdd4c4097f43cb6
Matheus Rian <souzamatheusrian@gmail.com>
2022-07-16 19:25:29 -0300

    feat: ScoreCard Zustand

---
+++ src/components/Zustand/ScoreCard.tsx
@@ -0,0 +1,18 @@
+
+import { useStore } from '../../store/Zustand';
+
+export const ScoreCard = () => {
+  const score = useStore(s => s.user.score);
+  const updateUser = useStore(s => s.updateUser);
+
+  return (
+    <div>
+      <input 
+        type="number" 
+        value={score}
+        onChange={(ev) => updateUser({ score: Number(ev.target.value) })}
+        placeholder="ScoreCard"
+      />
+    </div>
+  )
+}
